7d0b2f0b20f82b2980ab52095b6a21600451b285
Javier Amaya <Javieramayapat@gmail.com>
2023-03-30 09:55:58 -0600

    fix[database]: Fix typo in sql in task declaration, and delte trailin coma in models.py

--- db/tasks.sql
+++ db/tasks.sql
@@ -14,7 +14,7 @@ CREATE TABLE users(
 );
 
 
-CREATE TABLE taks (
+CREATE TABLE tasks (
     id bigserial NOT NULL,
     user_id bigint NOT NULL,
     title VARCHAR(150) NOT NULL,
@@ -29,17 +29,17 @@ CREATE TABLE taks (
 INSERT INTO users (id, name, last_name, username, email, password) VALUES (1, 'Javier', 'Amaya','javieramayapat', 'javieramayapat@gmail.com', 'easypassword');
 
 
-INSERT INTO taks (user_id, title, description, due_date, status)
+INSERT INTO tasks (user_id, title, description, due_date, status)
 VALUES (1, 'Finish report', 'Complete the report on project progress', '2023-04-05', 'in progress');
 
-INSERT INTO taks (user_id, title, description, due_date, status)
+INSERT INTO tasks (user_id, title, description, due_date, status)
 VALUES (1, 'Buy groceries', 'Pick up milk, eggs, bread, and vegetables', '2023-04-01', 'completed');
 
-INSERT INTO taks (user_id, title, description, due_date, status)
+INSERT INTO tasks (user_id, title, description, due_date, status)
 VALUES (1, 'Call client', 'Schedule a meeting with the new client', '2023-04-08', 'pending');
 
-INSERT INTO taks (user_id, title, description, due_date, status)
+INSERT INTO tasks (user_id, title, description, due_date, status)
 VALUES (1, 'Prepare presentation', 'Create slides for upcoming presentation', '2023-04-12', 'required');
 
-INSERT INTO taks (user_id, title, description, due_date, status)
+INSERT INTO tasks (user_id, title, description, due_date, status)
 VALUES (1, 'Submit timesheet', 'Complete and submit weekly timesheet', '2023-04-02', 'completed');
